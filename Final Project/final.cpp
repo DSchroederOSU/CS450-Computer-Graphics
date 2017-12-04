257c965c1bc4e9c18d7c01430ffe62f24fbe9860
DSchroederOSU <schrodan@oregonstate.edu>
2017-12-03 17:29:53 -0800

    almost done

--- Final Project/final.cpp
+++ Final Project/final.cpp
@@ -5440,10 +5440,39 @@ Display( )
                     drawStreet(12 - i, 12 - j);
                     glPopMatrix( );
                 }
-
-
-
 			}
+            else if(town[i][j] == 2){
+                glPushMatrix( );
+                glScalef(3, 1, 3);
+                glTranslatef((12 - i), 0.01, (12 - j));
+                glEnable(GL_TEXTURE_2D);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+                glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+                glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+                glTexImage2D(GL_TEXTURE_2D, 0, 3, texture_w, texture_h, 0, GL_RGB, GL_UNSIGNED_BYTE, texture);
+
+                //build road
+                glPushMatrix();
+                glBegin( GL_QUADS );
+                glColor3f( 0., 0., 0. );
+                glNormal3f( 0., 1.,  0. );
+                glTexCoord2f( 0, 0 );
+                glVertex3f( 0.5, 0, -0.5);
+                glTexCoord2f( 0, 0.1 );
+                glVertex3f( 0.5, 0, 0.5);
+                glTexCoord2f( 0.1, 0.1 );
+                glVertex3f( -0.5, 0, 0.5);
+                glTexCoord2f( 0.1, 0 );
+                glVertex3f( -0.5, 0, -0.5);
+                glEnd( );
+                glPopMatrix();
+                glDisable(GL_TEXTURE_2D);
+                glPopMatrix( );
+
+            }
 			else if(town[i][j] == 3){
                 glPushMatrix( );
                     glScalef(3, 1, 3);
